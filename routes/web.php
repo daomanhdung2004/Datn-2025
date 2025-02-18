5557eb21f3530a7638a9d9ca7a9f0399b9e8701b
daomanhdung2004 <dungdmph35024@fpt.edu.vn>
2025-02-18 15:05:29 +0700

    Login and regiter

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\restapi\AuthApi;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,29 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthApi::class, 'login'])->name('auth.login');
+    Route::post('/register', [AuthApi::class, 'register'])->name('auth.register');
+    Route::get('/logout', [AuthApi::class, 'logout'])->name('auth.logout');
+    Route::post('/forgot_password', [AuthApi::class, 'forgotPassword'])->name('auth.forgot.password');
+    Route::post('/change_password', [AuthApi::class, 'changePassword'])->name('auth.change.password');
+});
+
+// /* Restapi api */
+// Route::group(['prefix' => 'api'], function () {
+//     require_once __DIR__ . '/api/restapi.php';
+// });
+
+// /* Auth api */
+// Route::group(['prefix' => 'api', 'middleware' => ['auth.api']], function () {
+//     require_once __DIR__ . '/api/auth.php';
+// });
+
+// /* Admin api */
+// Route::group(['prefix' => 'api/admin', 'middleware' => ['admin.api']], function () {
+//     require_once __DIR__ . '/api/admin.php';
+// });
